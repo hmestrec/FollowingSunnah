04a4abc579716e40b36b59595f66e145f654ebae
Hector <hectormestre1234@gmail.com>
2024-04-06 03:22:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// script.js
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none"; // Hide all elements with class="tabcontent"
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block"; // Show the current tab
+    evt.currentTarget.className += " active"; // Highlight the current tab
+  }
+  

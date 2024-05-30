e3bc72b8fd32f837a7ebb6d73c1b527db8913b79
Hector <hectormestre1234@gmail.com>
2024-05-30 16:45:09 -0500

    New commit

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
 function openTab(evt, tabName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
-// Add event listeners once the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-  // Get all tab buttons and add click event listeners
-  var tabs = document.querySelectorAll('.tablinks');
-  tabs.forEach(tab => {
-      tab.addEventListener('click', function(event) {
-          openTab(event, tab.getAttribute('onclick').split("'")[1]);
-      });
-  });
-
-  // Automatically click the first tab to load it by default
-  if (tabs.length > 0) {
-      tabs[0].click();
-  }
+// Set the default tab to be open
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("Videos").style.display = "block";
+    document.querySelector(".tablinks").className += " active";
 });
 
 // scripts.js
@@ -57,5 +43,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
-
-

1538b26264bb2342509a2ba2aa7583ac0550ee52
Hector <hectormestre1234@gmail.com>
2024-05-30 19:16:08 -0500

    best ever now

--- app.js
+++ app.js
@@ -33,6 +33,47 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Set the default tab to be open
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("Videos").style.display = "block";
+    document.querySelector(".tablinks").className += " active";
+});
+
+// Function to hide tab contents when home button is clicked
+function hideTabs() {
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+}
+
+// Add event listener to the home button
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("homeButton").addEventListener("click", hideTabs);
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const videoLinks = document.querySelectorAll('.video-link');
     
